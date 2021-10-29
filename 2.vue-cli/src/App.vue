05a44f7565473cb69f048512a05ec0f211e57c70
jackson.chen <jackson.chen@lunarax.io>
2021-10-29 17:33:41 +0800

    Move folder

---
+++ 2.vue-cli/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <MyDiv msg="hello"></MyDiv>
+</template>
+
+<script>
+import MyDiv from './components/MyDiv.vue'
+
+export default {
+  components: {
+    MyDiv
+  }
+}
+</script>
---
+++ 2.vue-cli/src/components/MyDiv.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['msg'],
+}
+</script>
