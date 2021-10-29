2af894b13efb60928f6c91eb32bb1ae7391f618e
jackson.chen <jackson.chen@lunarax.io>
2021-10-29 17:29:04 +0800

    Add vue-cli project

---
+++ 3.vue-project/1.custom/App.vue
@@ -0,0 +1,17 @@
+
+<script>
+  export default {
+    data() {
+      return {
+        greeting: 'Hello World!'
+      }
+    }
+  }
+</script>
+
+<template>
+  <div id="app">
+   <p>{{ greeting }}</p>
+   <div>ddddd</div>
+  </div>
+</template>
